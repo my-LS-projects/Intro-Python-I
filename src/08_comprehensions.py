ae3bb3ff0be9505359acd31550eca4200855e615
Dakotah Huey <hueydakotah@gmail.com>
2020-02-11 18:56:35 -0500

    comprehensions

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -26,7 +26,7 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [i.upper() for i in a]
 
 print(y)
 
@@ -37,5 +37,11 @@ x = input("Enter comma-separated numbers: ").split(",")
 
 # What do you need between the square brackets to make it work?
 y = []
+
+for i in x:
+    if int(i) % 2 == 0:
+        y.append(i)
+    else:
+        pass
 
 print(y)
